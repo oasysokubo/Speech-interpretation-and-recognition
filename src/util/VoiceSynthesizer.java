5b4f3e70716eccd1e4e59eae6c232a69706602ec
Oasys Okubo <oasysokubo@gmail.com>
2018-06-19 11:09:44 -0700

    main v

---
+++ src/util/VoiceSynthesizer.java
@@ -0,0 +1,70 @@
+package api;
+
+import java.util.Locale;
+import javax.speech.Central;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+
+/**
+ * VoiceSynthesizer class uses a Speech Synthesizer System, a text-to-speech 
+ * system able to convert normal language text into speech in symbolic
+ * linguistic representations like phonetic transcriptions into speech.
+ * 
+ * All rights go to Oracle's freetts-1 library.
+ */
+public class VoiceSynthesizer {
+	
+	// true if synthesizer has been launched
+	private boolean VoiceSynthesizerThreadRunnning = false;
+	
+	public VoiceSynthesizer() {
+		System.err.println("Error: Missing parameter");
+		System.exit(1);
+	}
+	
+	public VoiceSynthesizer(String s) {		
+		
+		if(s == null) {
+			System.err.println("Nothing to say.");
+			System.exit(1);
+		}
+		
+		// Start VoiceSynthesizer
+		textSpeech(s);
+	}
+	
+	public void textSpeech(String speech) {
+		try {
+			
+			if(!VoiceSynthesizerThreadRunnning) {
+				// Set Property as Kevin Dictionary
+				System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+				
+				// Register Engine
+				Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+				
+				// Create Synthesizer
+				Synthesizer synthesizer = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+				
+				// Thread Runnning
+				VoiceSynthesizerThreadRunnning = true;
+				
+				// Allocate Synthesizer
+				synthesizer.allocate();
+				
+				// Resume Synthesizer
+				synthesizer.resume();
+				
+				// Speak the speech text until queue is empty.
+				synthesizer.speakPlainText(speech, null);
+				synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
